ae82cc0ed79398550b3bf6aa669e034156385580
Muaath Alqarni <muaath1428@hotmail.com>
2025-07-17 13:18:38 +0300

    asdsf

--- build.py
+++ build.py
@@ -83,6 +83,7 @@ def secret_files():
     # Fetch info.json
     info_content = fetch_file(token, mu_repo, 'info.json')
     if not info_content:
+        print(token)
         print('Error: Failed to fetch info.json')
         return
     storages = json.loads(info_content.decode('utf-8'))
